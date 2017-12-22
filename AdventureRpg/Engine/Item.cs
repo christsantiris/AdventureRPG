4ddd83da5f17300369bb232014e8df7f9849d8a8
Chris Tsantiris <chris.tsantiris@gmail.com>
2017-12-22 12:09:58 -0500

    gold prices added to items

--- AdventureRpg/Engine/Item.cs
+++ AdventureRpg/Engine/Item.cs
@@ -5,12 +5,14 @@
         public int ID { get; set; }
         public string Name { get; set; }
         public string NamePlural { get; set; }
+        public int Price { get; set; }
 
-        public Item(int id, string name, string namePlural)
+        public Item(int id, string name, string namePlural, int price)
         {
             ID = id;
             Name = name;
             NamePlural = namePlural;
+            Price = price;
         }
     }
 }
--- AdventureRpg/Engine/Weapon.cs
+++ AdventureRpg/Engine/Weapon.cs
@@ -5,7 +5,8 @@
         public int MinimumDamage { get; set; }
         public int MaximumDamage { get; set; }
 
-        public Weapon(int id, string name, string namePlural, int minimumDamage, int maximumDamage) : base(id, name, namePlural)
+        public Weapon(int id, string name, string namePlural, int minimumDamage, int maximumDamage, int price)
+            : base(id, name, namePlural, price)
         {
             MinimumDamage = minimumDamage;
             MaximumDamage = maximumDamage;
